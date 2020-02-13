767c6dd56330e03a9d3397691e5cf577941bce01
nirtzur <nirtzu89@gmail.com>
2020-02-13 19:35:26 +0100

    remove articles plus confirmation methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,6 +29,12 @@ class ArticlesController < ApplicationController
             render 'edit'
         end
      end
+     def destroy
+        @article=Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = 'article removed'
+        redirect_to articles_path
+     end
      private
      def article_params
          params.require(:article).permit(:title,:description)
