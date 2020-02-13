8aa737095f118045777660dc4552fc0b4aa55935
nirtzur <nirtzu89@gmail.com>
2020-02-13 19:21:56 +0100

    listing articles plus show and edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
-
+    def index
+        @articles = Article.all
+    end
     def new
        @article = Article.new 
     end
@@ -14,6 +16,18 @@ class ArticlesController < ApplicationController
      end
      def show
          @article=Article.find(params[:id])
+     end
+     def edit
+         @article=Article.find(params[:id])
+     end
+     def update
+        @article=Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = 'article was updated'
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
      end
      private
      def article_params
